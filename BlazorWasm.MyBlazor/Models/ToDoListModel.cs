9f5b28f4f9238b8ab683e291604e24c06c6dddc0
ZinLinnhtet <mackk5504@gmail.com>
2024-02-02 22:19:38 +0630

    -

---
+++ BlazorWasm.MyBlazor/Models/ToDoListModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWasm.MyBlazor.Models
+{
+    public class ToDoListModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public bool IsCompleted { get; set; }
+        public string TaskName { get; set; }
+    }
+}
