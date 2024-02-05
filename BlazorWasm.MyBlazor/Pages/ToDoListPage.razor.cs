3ffba9b51239606f247754abbe7b740c6bb07a45
ZinLinnhtet <mackk5504@gmail.com>
2024-02-05 23:57:25 +0630

    -

--- BlazorWasm.MyBlazor/Pages/ToDoListPage.razor.cs
+++ BlazorWasm.MyBlazor/Pages/ToDoListPage.razor.cs
@@ -6,12 +6,16 @@ namespace BlazorWasm.MyBlazor.Pages
     {
         private static List<ToDoListModel> data = new List<ToDoListModel>();
         private ToDoListModel reqModel { get; set; } = new ToDoListModel();
-        public void Add()
+
+        private void Add()
         {
+            if (reqModel.TaskName == null)
+                return;
             data.Add(reqModel);
             reqModel = new ToDoListModel();
         }
-        public void Cancel()
+
+        private void Cancel()
         {
             reqModel = new ToDoListModel();
         }
@@ -21,7 +25,7 @@ namespace BlazorWasm.MyBlazor.Pages
             data[index].IsCompleted = !data[index].IsCompleted;
             Console.WriteLine(data[index].IsCompleted);
         }
-        public void Remove(string id)
+        private void Remove(string id)
         {
             data = data.Where(x => x.Id != id).ToList();
         }
